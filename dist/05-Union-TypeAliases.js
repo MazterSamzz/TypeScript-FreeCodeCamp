9f3f74d2e351e2ef4473b73ba010d2fb3f399fa8
Samuel Ivan <maztersamzz@gmail.com>
2021-11-01 05:55:53 +0700

    TypeScript 05 Union & Type Alias

---
+++ dist/05-Union-TypeAliases.js
@@ -0,0 +1,16 @@
+"use strict";
+// type ID = string
+// type PopularTag = string
+// type MaybePopularTag = PopularTag | null
+// interface UserInterface {
+// 	id: ID
+// 	name: string
+// 	surname: string
+// }
+// const popularTags: PopularTag[] = ['dragon', 'coffee']
+// const dragonsTag: MaybePopularTag = 'dragon'
+// let username: string = 'alex'
+// let pageName: string | number = '1'
+// let errorMessage: string | null = null
+// let user: UserInterface | null = null
+// let someProp: string | number | null | undefined | string[]
