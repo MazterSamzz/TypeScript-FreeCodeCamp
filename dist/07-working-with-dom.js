34aa056c7c8f9f4b2945114650dc801f6ca4057e
Samuel Ivan <maztersamzz@gmail.com>
2021-11-02 19:44:13 +0700

    TypeScript 07 Working with DOM

---
+++ dist/07-working-with-dom.js
@@ -0,0 +1,8 @@
+"use strict";
+// let page: any = '1'
+// let pageNumber = page as string
+// const someElement = document.querySelector('.foo')
+// someElement.addEventListener('blur', (event) => {
+// 	const target = event.target as HTMLInputElement
+// 	console.log('event', target.value)
+// })
