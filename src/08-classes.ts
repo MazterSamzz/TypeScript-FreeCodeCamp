1ae4a978d1c73574b7fd5dc164e84c38900f4112
Samuel Ivan <maztersamzz@gmail.com>
2021-11-03 06:33:22 +0700

    TypeScript 08 Classes

---
+++ src/08-classes.ts
@@ -0,0 +1,45 @@
+// interface UserInterface {
+// 	getFullName(): string
+// }
+
+// class User implements UserInterface {
+// 	firstName: string
+// 	lastName: string
+// 	readonly unchangeableName: string
+// 	static readonly maxAge = 50
+
+// 	constructor(firstName: string, lastName: string) {
+// 		this.firstName = firstName
+// 		this.lastName = lastName
+// 		this.unchangeableName = firstName
+// 	}
+
+// 	changeUnchangeableName(): void {
+// 		// this.unchangeableName = foo
+// 	}
+
+// 	getFullName(): string {
+// 		return this.firstName + ' ' + this.lastName
+// 	}
+// }
+
+// class Admin extends User {
+// 	private editor!: string
+
+// 	// 1:09:15
+// 	setEditor(editor: string): void {
+// 		this.editor = editor
+// 	}
+
+// 	getEditor(): string {
+// 		return this.editor
+// 	}
+// }
+
+// const user = new User('Monster', 'Lessons')
+// console.log(user.getFullName())
+// console.log(User.maxAge)
+
+// const admin = new Admin('Foo', 'Bar')
+// console.log(admin.firstName)
+// console.log(admin.getFullName)
